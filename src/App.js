13c44d4d8e3c5ff505d719811f0442f7fadf88e6
DESKTOP-KK4PQT7\raiot <davidh@altusjobs.com>
2021-01-28 11:19:52 -0500

    push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
+import Dropdown from './components/Dropdown';
+import Header from './components/Header';
+import Route from './components/Route';
+import Search from './components/Search';
+import Translate from './components/Translate';
 
 const items = [
     {
@@ -16,10 +21,38 @@ const items = [
     }
 ]
 
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The Color Blue',
+        value: 'blue'
+    }
+]
+
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
 
     return (
         <div>
-            <Accordion items={items} />
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown label="Select a Color" options={options} selected={selected} onSelectedChange={setSelected} />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>)
 }
